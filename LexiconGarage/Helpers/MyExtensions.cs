370bd4c4d19e6d80cbcee99937234b3ff19f64ee
Xgor <real.xgor@hotmail.com>
2026-06-17 08:19:11 +0200

    Improved Extension description

--- LexiconGarage/Helpers/MyExtensions.cs
+++ LexiconGarage/Helpers/MyExtensions.cs
@@ -3,13 +3,13 @@ namespace LexiconGarage.Helpers;
 public static class MyExtensions
 {
     /// <summary>
-    /// checks every T for 
+    /// checks every T if that specific variable is set to that specific value 
     /// </summary>
     /// <param name="data">IEnumerable list to filter</param>
     /// <param name="property">Name of the variable to filter by</param>
     /// <param name="attribute">value to check equals to</param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <typeparam name="T">Type to iterate by</typeparam>
+    /// <returns>Yield all T that has variable at that value</returns>
     public static IEnumerable<T> FilterByPropertyAttribute<T>(this IEnumerable<T> data, string property, string attribute)
     {
         foreach(T value in data)
